f5a1604dc2e62b07c8974ad9265a0e78c0a5482a
AlonsoVasquezDA <alonsovasquez1409@gmail.com>
2025-07-11 22:28:03 -0500

    primer commit

---
+++ PROYECTASO.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct CORREO {
+    string user;
+    string domain;
+};
+
+struct CUENTA {
+    string numcuenta;
+    string contra;
+    double din;
+};
+
+struct DATOS {
+    string nom;
+    int    edad;
+    string dni;
+    string celular;
+    CORREO correo;
+    CUENTA cuenta;
+};
+
+
+int main (){
+     int op;
+     do{
+        cout<<"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"<<endl;
+        cout<<"---------------BIENVENIDO A BANCO BCHP-----------------"<<endl<<endl;
+        cout<<" 1.- Crear una cuenta. "<<endl;
+        cout<<" 2.- Ingresar una cuenta. "<<endl;
+        cout<<" 3.- Salir."<<endl<<endl;
+        cout<<"Ingrese una de las opciones: ";
+        cout<<endl;
+        cin>>op;
+        switch (op){
+            case 1: 
+                   break;
+            case 2:
+                   break;
+            case 3: 
+                   char ops;
+                   cout<<" Estas seguro de salir? (S/N)"<<endl;
+                   cin>>ops;
+                   if (ops == 'S'){
+                      op = 3;
+                      cout<<"Gracias por confiar en el BANCO BCHP. HASTA PRONTO."<<endl;
+                   } else {
+                      op = -1;
+                   }
+                   break;     
+            default :
+                   cout<<"Opcion invalida."<<endl;
+                   system ("pause");
+                   break;
+        }
+     } while (op != 3);
+     return 0;
+}
